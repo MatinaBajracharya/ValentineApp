9726299bc559aa8a784a3e6d09c8cddff4d4b58e
Matina Bajracharya <matina@Matinas-MacBook-Pro.local>
2026-02-11 20:05:05 +1100

    add: rejected case

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import { useState } from "react";
 import "./App.css";
+import besos from "./assets/besos.gif";
+import cute from "./assets/cute2.jpg";
+import punch from "./assets/punch.gif";
 
 function App() {
   // const [name, setName] = useState("");
-  const [noPosition, setNoPosition] = useState({ top: "50%", left: "55%" });
+  // const [noPosition, setNoPosition] = useState({ top: "50%", left: "55%" });
   const [accepted, setAccepted] = useState(false);
+  const [rejected, setRejected] = useState(false);
 
-  const moveNoButton = () => {
-    const top = Math.floor(Math.random() * 80) + "%";
-    const left = Math.floor(Math.random() * 80) + "%";
-    setNoPosition({ top, left });
-  };
+  // const moveNoButton = () => {
+  //   const top = Math.floor(Math.random() * 80) + "%";
+  //   const left = Math.floor(Math.random() * 80) + "%";
+  //   setNoPosition({ top, left });
+  // };
 
   return (
     <div className="container">
-      {!accepted ? (
+      {!accepted && !rejected ? (
         <>
           <h1>💖 On the occassion of Valentine’s Day 💖</h1>
 
@@ -26,26 +30,32 @@ function App() {
           /> */}
 
           <h2> Salim, will you be my Valentine? 💌</h2>
-          <img src="cute.png" alt="cute.png" />
+          <img src={cute} alt="cute.png" style={{height:200, width:500}}/>
 
           <div className="buttons">
             <button className="yes" onClick={() => setAccepted(true)}>
               Yes 💘
             </button>
 
-            <button
+            {/* <button
               className="no"
               style={{ top: noPosition.top, left: noPosition.left }}
               onMouseEnter={moveNoButton}
-            >
+            > */}
+            <button className="no" onClick={() => setRejected(true)}>
               No 😜
             </button>
           </div>
         </>
-      ) : (
+      ) : accepted? (
         <>
           <h1>🎉 Yay! Happy Valentine’s Day 💕I Love You So Much!</h1>
-          <img src="/besos.gif" alt="Besos gif" />
+          <img src={besos} alt="Besos gif" />
+        </>
+      ): (
+        <>
+          <h1>La kha 😡</h1>
+          <img src={punch} alt="punch" />
         </>
       )}
     </div>
